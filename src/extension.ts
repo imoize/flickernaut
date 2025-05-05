32e799c3d03ab6baae9d5b1b390dadc3810fd3ce
imoize <51510865+imoize@users.noreply.github.com>
2025-05-05 23:58:38 +0700

    chore: implement initial extension and preferences classes

---
+++ src/extension.ts
@@ -0,0 +1,13 @@
+import Gio from 'gi://Gio';
+import GLib from 'gi://GLib';
+import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
+
+export default class Flickernaut extends Extension {
+    enable() {
+
+    }
+
+    disable() {
+
+    }
+}
---
+++ src/prefs.ts
@@ -0,0 +1,9 @@
+import type Gio from 'gi://Gio';
+import Adw from 'gi://Adw';
+import GLib from 'gi://GLib';
+import Gtk from 'gi://Gtk';
+import { gettext as _, ExtensionPreferences } from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
+
+export default class FlickernautPrefs extends ExtensionPreferences {
+
+}
